8d49aa24bdf16b2fa3d36b2111ea241c726d4469
Yashin <yashinnaran@gmail.com>
2023-11-24 13:17:22 +0200

    Article Card desktop layout complete.

--- src/components/articleCard/ArticleCard.js
+++ src/components/articleCard/ArticleCard.js
@@ -1,29 +1,52 @@
 import styles from "./ArticleCard.module.css";
 
 import drawerImg from "../../images/drawers.jpg";
+import profielImg from "../../images/avatar-michelle.jpg";
+import btnIcon from "../../images/icon-share.svg";
+import facebookIcon from "../../images/icon-facebook.svg";
+import twitterIcon from "../../images/icon-twitter.svg";
+import pinterestIcon from "../../images/icon-pinterest.svg";
 
 const ArticleCard = () => {
   return (
     <div className={styles.articleCard}>
       <img src={drawerImg} alt="drawers" className={styles.drawerImg} />
-      <section>
-        <article>
+      <section className={styles.articleWrapper}>
+        <article className={styles.article}>
           <h2>
             Shift the overall look and feel by adding these wonderful touches to
             furniture in your home
           </h2>
 
           <p>
-            ver been in a room and felt like something was missing? Perhaps it
+            Ever been in a room and felt like something was missing? Perhaps it
             felt slightly bare and uninviting. I’ve got some simple tips to help
             you make any room feel complete.
           </p>
         </article>
 
-        <div>
-          <img src="" alt="" />
-          <h3>Michelle Appleton</h3>
-          <p>28 Jun 2020</p>
+        <div className={styles.profileContent}>
+          <div className={styles.profileWrapper}>
+            <img src={profielImg} alt="profile image" />
+
+            <div>
+              <h3>Michelle Appleton</h3>
+              <p>28 Jun 2020</p>
+            </div>
+          </div>
+
+          <div className={styles.shareWrapper}>
+            <button className={styles.btnShare}>
+              <img src={btnIcon} alt="" />
+            </button>
+
+            <aside className={styles.shareMenu}>
+              <h3>share</h3>
+              <img src={facebookIcon} alt="Facebook logo" />
+              <img src={twitterIcon} alt="Twitter logo" />
+              <img src={pinterestIcon} alt="Pinterest logo" />
+            </aside>
+          </div>
         </div>
       </section>
     </div>
